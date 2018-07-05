a837b33628f8a78d647ae86743a6bb4684b54406
marquedante <marquedante@yahoo.com.br>
2018-07-05 17:36:50 -0300

    commit

--- MavenDesktop/src/main/java/br/com/model/principal/Principal.java
+++ MavenDesktop/src/main/java/br/com/model/principal/Principal.java
@@ -9,18 +9,14 @@ public class Principal {
 		Aluno al = new Aluno("Caio","12JE0",new Integer(12),"123456");
 		System.out.println(al.getNome()+" - "+al.getCodigo()+" - "+al.getIdade());
 		
-<<<<<<< HEAD
 		
 		Aluno al1 = new Aluno("Diogo","RT90",new Integer(7),"987433");
 		System.out.println(al1.getNome()+" - "+al1.getCodigo()+" - "+al1.getIdade());
 		
 		
 		Aluno al2 = new Aluno("Renata","QW23",new Integer(8),"785456");
-=======
-		Aluno al2 = new Aluno("Renata","QW23",new Integer(8));
->>>>>>> branch 'master' of https://github.com/Marquedante/DesktopRepository.git
 		System.out.println(al2.getNome()+" - "+al2.getCodigo()+" - "+al2.getIdade());
 		
 	}
-
+ 
 }
