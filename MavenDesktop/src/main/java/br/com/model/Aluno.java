e5a704af64c43907c5da24afc36fd842d06240ad
marquedante <marquedante@yahoo.com.br>
2018-07-05 17:12:42 -0300

    commit

--- MavenDesktop/src/main/java/br/com/model/Aluno.java
+++ MavenDesktop/src/main/java/br/com/model/Aluno.java
@@ -1,6 +1,10 @@
 package br.com.model;
 
-public class Aluno {
+import java.io.Serializable;
+
+public class Aluno implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
 	
 	private String nome;
 	private String codigo;
