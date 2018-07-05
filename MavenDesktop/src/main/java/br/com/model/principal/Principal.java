8db5a908711a7b13e314d982f8b89756b4f3f1c4
Marquedante <marquedante@yahoo.com.br>
2018-07-05 16:09:15 -0300

    isercao de idade para ser impressa

--- MavenDesktop/src/main/java/br/com/model/principal/Principal.java
+++ MavenDesktop/src/main/java/br/com/model/principal/Principal.java
@@ -6,7 +6,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		Aluno al = new Aluno("Caio","12JE0");
+		Aluno al = new Aluno("Caio","12JE0",new Integer(9));
 		
 		System.out.println(al.getNome()+" - "+al.getCodigo());
 
