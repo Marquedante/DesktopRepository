a1b28cfbce366435bea032b2031e9da59d6e1dc9
Marquedante <marquedante@yahoo.com.br>
2018-07-05 17:12:13 -0300

    alteracao

--- MavenDesktop/src/main/java/br/com/model/principal/Principal.java
+++ MavenDesktop/src/main/java/br/com/model/principal/Principal.java
@@ -10,13 +10,18 @@ public class Principal {
 		
 		System.out.println(al.getNome()+" - "+al.getCodigo()+" / "+al.getIdade());
 		
+		/////////////////////////////////////////////////////////////
+		
 		Aluno al1 = new Aluno("Diogo","RT90",new Integer(7));
 		
 		System.out.println(al1.getNome()+" - "+al1.getCodigo()+" - "+al1.getIdade());
 		
+		//////////////////////////////////////////////////////////////
+		
 		Aluno al2 = new Aluno("Renata","QW23",new Integer(8));
 		
 		System.out.println(al2.getNome()+" - "+al2.getCodigo()+" - "+al2.getIdade());
+		
 	}
 
 }
