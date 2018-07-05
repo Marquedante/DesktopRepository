9e233b3fde185f419536dbae07a0c1ed04d51cab
marquedante <marquedante@yahoo.com.br>
2018-07-05 16:32:08 -0300

    commit

--- MavenDesktop/src/main/java/br/com/model/principal/Principal.java
+++ MavenDesktop/src/main/java/br/com/model/principal/Principal.java
@@ -8,12 +8,16 @@ public class Principal {
 		
 		Aluno al = new Aluno("Caio","12JE0",new Integer(12));
 		
-		System.out.println(al.getNome()+" - "+al.getCodigo());
+		System.out.println(al.getNome()+" - "+al.getCodigo()+" - "+al.getIdade());
 		
 		Aluno al1 = new Aluno("Diogo","RT90",new Integer(7));
 		
-		System.out.println(al1.getNome()+" - "+al1.getCodigo());
+		System.out.println(al1.getNome()+" - "+al1.getCodigo()+" - "+al.getIdade());
 
+		Aluno al2 = new Aluno("Renata","YU09",new Integer(3));
+		
+		System.out.println(al2.getNome()+" - "+al2.getCodigo()+" - "+al.getIdade());
+		
 
 	}
 
