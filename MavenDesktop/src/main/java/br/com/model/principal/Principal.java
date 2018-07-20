5a38b4a1802da488afe5b578c43034968f0e2440
marquedante <marquedante@yahoo.com.br>
2018-07-20 19:47:16 -0300

    Alteracao no Principal

--- MavenDesktop/src/main/java/br/com/model/principal/Principal.java
+++ MavenDesktop/src/main/java/br/com/model/principal/Principal.java
@@ -9,13 +9,15 @@ public class Principal {
 		Aluno al = new Aluno("Caio","12JE0",new Integer(12),"123456");
 		System.out.println(al.getNome()+" - "+al.getCodigo()+" - "+al.getIdade());
 		
-		
 		Aluno al1 = new Aluno("Diogo","RT90",new Integer(7),"987433");
 		System.out.println(al1.getNome()+" - "+al1.getCodigo()+" - "+al1.getIdade());
 		
-		
 		Aluno al2 = new Aluno("Renata","QW23",new Integer(8),"785456");
 		System.out.println(al2.getNome()+" - "+al2.getCodigo()+" - "+al2.getIdade());
+		
+		Aluno al3 = new Aluno("Flavia","PY78",new Integer(7),"65090");
+		System.out.println(al3.getNome()+" - "+al3.getCodigo()+" - "+al3.getIdade());
+		
 		
 	}
  
