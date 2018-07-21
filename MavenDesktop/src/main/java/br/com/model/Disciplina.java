e968b770d586c4a988b0bfef6dc9937aa7fb1022
marquedante <marquedante@yahoo.com.br>
2018-07-20 21:50:13 -0300

    implementacao do metodos equals e hashcode.

--- MavenDesktop/src/main/java/br/com/model/Disciplina.java
+++ MavenDesktop/src/main/java/br/com/model/Disciplina.java
@@ -38,5 +38,31 @@ public class Disciplina {
 	public void setCodigo(String codigo) {
 		this.codigo = codigo;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Disciplina other = (Disciplina) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 }
 	
